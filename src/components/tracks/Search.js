83584a36e06bcfb8a63dcfec4658313f3087a45a
Carlos Lantigua <clantigua954@gmail.com>
2018-11-02 11:17:54 -0400

    completed search function

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -10,17 +10,38 @@ class Search extends Component {
 	onChange = (e) => {
 		this.setState({ [e.target.name]: e.target.value });
 	};
+
+	findTrack = (dispatch, e) => {
+		e.preventDefault();
+		axios
+			.get(
+				`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state
+					.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
+			)
+			.then((res) => {
+				dispatch({
+					type: 'SEARCH_TRACKS',
+					payload: res.data.message.body.track_list
+				});
+				this.setState({ trackTitle: '' });
+				// console.log(res.data);
+				// this.setState({ track_list: res.data.message.body.track_list });
+			})
+			.catch((err) => console.log(err));
+	};
+
 	render() {
 		return (
 			<Consumer>
 				{(value) => {
+					const { dispatch } = value;
 					return (
 						<div className="card card-body mb-4 p4">
 							<h1 className="display-4 text-center">
 								<i className="fas fa-music" /> Search For A Song
 							</h1>
 							<p className="lead text-center">Get the Lyrics for any song</p>
-							<form>
+							<form onSubmit={this.findTrack.bind(this, dispatch)}>
 								<div className="form-group">
 									<input
 										type="text"
