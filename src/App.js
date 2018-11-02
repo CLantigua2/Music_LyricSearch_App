7a49386a8dcf3ebd74e22b77b6bf335719268050
Carlos Lantigua <clantigua954@gmail.com>
2018-11-02 09:55:25 -0400

    set route to lyrics page, get lyrics and track info

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
 import { Provider } from './context';
+import Lyrics from './components/tracks/Lyrics';
 
 class App extends Component {
 	render() {
@@ -13,6 +14,7 @@ class App extends Component {
 				<div className="container">
 					<Switch>
 						<Route exact path="/" component={Index} />
+						<Route exact path="/lyrics/track/:id" component={Lyrics} />
 					</Switch>
 				</div>
 			</Provider>
