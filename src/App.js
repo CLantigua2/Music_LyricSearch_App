2a2e2cb854dd8f2e2fd1d5521adc892a2550acc1
Carlos Lantigua <clantigua954@gmail.com>
2018-11-02 07:18:06 -0400

    added context and mapped out track information

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+import { Provider } from './context';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<h1>Hello World</h1>
-			</div>
+			<Provider>
+				<Navbar />
+				<div className="container">
+					<Switch>
+						<Route exact path="/" component={Index} />
+					</Switch>
+				</div>
+			</Provider>
 		);
 	}
 }
